74b39d8c69ed2f9dfe74b0dd3e92f286ddab5d2e
tkamanda <toelokemwa@gmail.com>
2024-07-21 17:16:25 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+window.customerName = "bob";
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    window.bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "some customer";
+    leastFavoriteCustomer = "another customer";
+}
